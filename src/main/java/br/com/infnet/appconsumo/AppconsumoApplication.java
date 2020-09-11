fc881015193bdf35e553f6509c6efe6747f23000
vitor-bastos <vitor.bastos.silva@gmail.com>
2020-09-11 17:51:27 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/infnet/appconsumo/AppconsumoApplication.java
@@ -0,0 +1,14 @@
+package br.com.infnet.appconsumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class AppconsumoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppconsumoApplication.class, args);
+	}
+}
